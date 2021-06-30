29adaf849ea2ab94acaae511355d3ce8932c08bc
Roland Csibrei <roland.csibrei@nascor.tech>
2021-07-01 01:47:32 +0200

    Added some comments

--- src/components/BabylonJsScene.vue
+++ src/components/BabylonJsScene.vue
@@ -20,18 +20,28 @@ const BJS_CANVAS_ZINDEX = 9;
 
 export default {
   setup() {
+    // the variable name MUST match the ref value used in the html templte above
     const bjsCanvas = ref(null);
     let resizeListener = null;
 
+    // the canvas is accessible only when the component is already mounted
     onMounted(() => {
+      // you have to set the width on the canvas object
+      // setting the width and height using css or canvas.style
+      // only stretches the canvas to the desired dimensions
       bjsCanvas.value.width = BJS_CANVAS_WIDTH;
       bjsCanvas.value.height = BJS_CANVAS_HEIGHT;
       bjsCanvas.value.style.zIndex = BJS_CANVAS_ZINDEX;
 
+      // do not forget to use the .value property on the ref object
+      // everwhere you need to access the canvas
       const engine = new Engine(bjsCanvas.value);
       const scene = new Scene(engine);
 
+      // create a scene
       createScene(scene, bjsCanvas.value);
+
+      // the render loop is actually rendering the scene
       setupRenderLoop(engine, scene);
 
       resizeListener = window.addEventListener("resize", () => {
@@ -39,10 +49,14 @@ export default {
       });
     });
 
-    onUnmounted(() => {
+    const cleanup = () => {
       if (resizeListener) {
         window.removeEventListener("resize", resizeListener);
       }
+    };
+
+    onUnmounted(() => {
+      cleanup();
     });
 
     const setupRenderLoop = (engine, scene) => {
@@ -52,6 +66,7 @@ export default {
     };
 
     const createScene = (scene, canvas) => {
+      // setting up a basic scene
       const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
       camera.setTarget(Vector3.Zero());
       camera.attachControl(canvas, true);
@@ -71,6 +86,7 @@ export default {
       MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
     };
 
+    // you need yo return the ref to the babylonJS canvas
     return {
       bjsCanvas,
     };
